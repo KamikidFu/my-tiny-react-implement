0e83485ad33a4fd07116a967632af63ba3405e6d
Kami <kidfu@live.com>
2020-08-02 19:06:21 +1200

    set up for tic tac toe clone

---
+++ tictactoe.js
@@ -0,0 +1,44 @@
+import { TinyReact, Component } from './TinyReact';
+
+//Tic tac toe clone
+class Square extends Component {
+	render() {
+		return (
+			<button className="square" onClick={() => alert('click')}>
+				{this.props.value}
+			</button>
+		);
+	}
+}
+
+class Board extends Component {
+	renderSquare(i) {
+		return <Square value={i} />;
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="board-row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		);
+	}
+}
+
+let node = <Board />;
+
+TinyReact.render(node, document.body);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	entry: {
-		index: './index.js'
+		index: './index.js',
+		tictactoe: './tictactoe.js'
 	},
 	mode: 'development',
 	optimization: {
